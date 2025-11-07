3058ff9498a561372306ea7d3bf8653b0d7db3ed
CharlieMollw <Jdds0921@Leeds.ac.uk>
2025-11-07 21:13:09 +0000

    Git up

--- grade_analyser_original.py
+++ grade_analyser_original.py
@@ -29,31 +29,38 @@ Your output files must be structured exactly as described - output files for all
 Note:
 Your code will only be tested on valid files in the format shown in the 4 example files in this folder - you do not need to validate any data.
 '''
-#name = input()
-with open("student_data_10.csv", "r") as infile:
-    data = [line.strip().split(",") for line in infile]
+name = input()
+with open(name, "r") as infile:
+   data = [line.strip().split(",") for line in infile]
 x = 0
 final = []
 for i in data:
-    if x == 0:
-     x=x+1
-    else:
-     sum = 0
-     count = 0
-     for a in range(1,i.count()):
-          if len(i[a]) > 0:
-             count = count+1
-             sum = sum+int(i[a])
-     avg = sum/count
-     if avg >= 70:
-        grade = "1"
-     elif avg >= 60:
-        grade = "2:1"
-     elif avg >= 50:
-        grade = "2:2"
-     elif avg >= 40:
-        grade = "3"
-     else:
-        grade = "F"
+   if x == 0:
+      x=x+1
+   else:
+      sum = 0
+      count = 0
+      for a in range(1,len(i)):
+         if len(i[a]) > 0:
+            count = count+1
+            sum = sum+int(i[a])
+      avg = sum/count
+      if avg >= 70:
+         grade = "1"
+      elif avg >= 60:
+         grade = "2:1"
+      elif avg >= 50:
+         grade = "2:2"
+      elif avg >= 40:
+         grade = "3"
+      else:
+         grade = "F"
+      final.append([i[0],f"{avg:.2f}",grade])
+with open((name+"_out"), "a") as outfile:
+   for i in final:
+      line = f"{i[0]},{i[1]},{i[2]}\n"
+      outfile.write(line)
      
+     
+
 
